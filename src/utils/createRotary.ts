749a89624e9355d6b2e3876795cdf364179774d2
Joris Gignoux <joris.gignoux@slatedigital.com>
2025-06-30 10:21:17 +0200

    EIOSIS_CHANGES: Add support for matrix export + set UI default values

--- src/utils/createRotary.ts
+++ src/utils/createRotary.ts
@@ -15,7 +15,13 @@ export const createRotary = (nbSteps: number, nbDegrees: number, frameName: stri
   const group = frame.children[0] as GroupNode;
   const { width, height } = frame;
 
-  frame.resizeWithoutConstraints((isVertical ? 1 : nbSteps) * width, (isVertical ? nbSteps : 1) * height);
+  if (isVertical) {
+    frame.resizeWithoutConstraints(width, nbSteps * height);
+  } else {
+    const numRow = Math.ceil(Math.sqrt(nbSteps));
+    const numCol = Math.floor(Math.sqrt(nbSteps));
+    frame.resizeWithoutConstraints(numRow * width, numCol * height);
+  }
 
   for (let i = 0; i < nbSteps; i++) {
     const rotationGroup = group.clone();
@@ -79,4 +85,23 @@ export const createRotary = (nbSteps: number, nbDegrees: number, frameName: stri
     frame.insertChild(i + 1, rotationGroup);
   }
   group.remove();
+
+  frame.layoutMode = isVertical ? "VERTICAL" : "HORIZONTAL";
+  frame.primaryAxisSizingMode = "AUTO";
+  frame.counterAxisSizingMode = "AUTO";
+  frame.layoutWrap = isVertical ? "NO_WRAP" : "WRAP";
+
+  const padding = 0;
+  frame.itemSpacing = padding * 2; // Space between items (optional)
+  frame.counterAxisSpacing = padding * 2;
+  frame.paddingLeft = padding;
+  frame.paddingRight = padding;
+  frame.paddingTop = padding * 2;
+  frame.paddingBottom = padding * 2;
+
+  const numRow = Math.ceil(Math.sqrt(nbSteps));
+  const numCol = Math.floor(Math.sqrt(nbSteps));
+  //frame.resizeWithoutConstraints(numRow * width, numCol * height);
+  frame.resize(numRow * width, numCol * height);
+
 }
--- src/utils/createRotationFrame.ts
+++ src/utils/createRotationFrame.ts
@@ -1,10 +1,12 @@
 export const createRotationFrame = (width: number, height: number, index: number, direction: 'vertical' | 'horizontal') => {
   const isHorizontal = direction === 'horizontal';
   const frame = figma.createFrame();
-  frame.resize(width, height);
+  frame.resize(1, 1);
   frame.x = isHorizontal ? index * width + width / 2 : width / 2;
   frame.y = isHorizontal ? height / 2 : index * height + height / 2;
   frame.clipsContent = false;
+  frame.strokes = [{ type: 'SOLID', color: { r: 1, g: 0, b: 0 } }];
+  frame.strokeWeight = 1;
   const fills = JSON.parse(JSON.stringify(frame.fills));
   fills[0].opacity = 0;
   frame.fills = fills;
